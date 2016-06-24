019747fa62334c88ae81bb220eaf37544ddf1916
arthurchan1111 <atchan2@buffalo.edu>
2016-06-24 17:48:07 -0400

    Added form validation library and mostly functional map
    Need to work on syncing firebase to map and login

--- js/register.js
+++ js/register.js
@@ -1,79 +1,195 @@
 $(document).ready(function(){
-var database= new Firebase("https://eventplanner-arthurchan.firebaseio.com");
-
-$("#emailinput").focusout(function(){
+	
+ var config = {
+    apiKey: "AIzaSyDlYhWf4GHAY75jlhyX-Fx4Ycy5-ZD9lFI",
+    authDomain: "eventplanner-arthurchan.firebaseapp.com",
+    databaseURL: "https://eventplanner-arthurchan.firebaseio.com",
+    storageBucket: "eventplanner-arthurchan.appspot.com",
+  };
+  firebase.initializeApp(config);
+$("#registration").formValidation({
+	framework: "bootstrap",
+	icon:{
+		valid:"glyphicon glyphicon-ok",
+		invalid:"glyphicon glyphicon-remove",
+		validating:"glyphicon glyphicon-refresh"
+	},
+	fields:{
+		fname:{
+			row:".col-md-6",
+			validators:{
+				notEmpty:{
+					message:"First Name is Required"
+				},
+				
+				regexp:{
+					regexp:/^[a-zA-Z]+$/,
+					message: "You cannot use any numbers or special characters in input"	
+					
+				}
+			}
+		},
+		lname:{
+			row: ".col-md-6",
+			validators:{
+				notEmpty:{
+					message:"Last Name is Required"
+				},
+				
+				regexp:{
+					regexp:/^[a-zA-Z]+$/,
+					message: "You cannot use any numbers or special characters in input"	
+					
+				}
+			}
+		},
+		
+		emailin:{
+			row:".col-md-12",
+			validators:{
+				notEmpty:{
+					message:"Please Enter an Email Address"
+				},
+				emailAddress:{
+					message:"This is not a valid Email Address"
+				}
+		/*	
+				callback:{
+					message:"This Email Address Is Already Registered",
+					callback: function(value,validator,$field){
+						var finalproduct;
+						$("#emailinput").focusout(function(){
+						var result;
+						var email= $("#emailinput").val();
+						firebase.database().ref("users/").orderByChild("email").equalTo(String(value)).on("child_added",function(snapshot){
+						result= snapshot.val().email;
+						
+						 
+						
+						});
+						 
+						 finalproduct =(value===result)? "false":"true";
+							console.log(finalproduct);
+						});
+						
+						
+						
+						return false;
+						
+				}
+			*/	
+				
+			}
+			
+		},
+		
+		passwordin:{
+			row: ".col-md-12",
+			validators:{
+				notEmpty:{
+					message:"Password is required"
+				},
+				stringLength:{
+							min:6,
+							max:30,
+							message:"Password Must Be Between 6 and 30 Characters"
+				},
+				
+				regexp:{
+					regexp: /^[a-zA-Z0-9]+$/,
+					message: "Password can only contain Letters and Numbers"
+					
+				}
+				
+				
+			}
+		},
+		confirmpasswordin:{
+			row: ".col-md-12",
+			validators:{
+				notEmpty:{
+					message:"You need to reenter your password"
+				},
+				callback:{
+					message: "This does not match with password entered",
+					callback: function(value,validator,$field){
+						var typedpassword = $("#pwdinput").val();
+						
+						return value == typedpassword;
+						
+					}
+					
+					
+				}
+				
+			}
+			
+			
+		}
+		
+		
+		
+	
+	
+	}
+});
+/*
+$("#emailinput").blur(function(){
+var result;
 var email= $("#emailinput").val();
-database.child("userIDAdmin");
-database.equalTo(String(email)).on("child_added", function(snapshot){
-console.log(snapshot.val().email); 
-
+firebase.database().ref("users/").orderByChild("email").equalTo(String(email)).on("child_added",function(snapshot){
+result= snapshot.val().email;
+//console.log(result);
+ if (result===email){
+	 $("#emailGroup").append(" <span class='glyphicon glyphicon-remove form-control-feedback'></span>")
+	 $("#emailGroup").toggleClass("form-group has-warning has-feedback");
+ }						 
+						
+});
+						
 });
 
-
-});
-
-$("#confirmpass").focusout(function(){
-var password= $("#pwdinput").val();
-
-var confirmPassword=$("#confirmpass").val();
-
-if(password === confirmPassword){
-
-$("#confirmPassGroup").toggleClass("form-group has-success has-feedback");
-$("#mutationConfirmPassword").toggleClass("glyphicon glyphicon-ok form-control-feedback");
-
-
-}
-else{
-
-$("#confirmPassGroup").toggleClass("form-group has-error has-feedback");
-$("#mutationConfirmPassword").toggleClass("glyphicon glyphicon-remove form-control-feedback");
-
-}
-
-
-
-
-
-});
-
+*/
 $("#registerConfirm").click(function(){
 var email= $("#emailinput").val();
-var password= $("#pwdinput").val();
+var pwd= $("#pwdinput").val();
 var fname = $("#firstnameInput").val();
 var lname = $("#lastnameInput").val();
-database.createUser({
-email: String(email),
-password: String(password)
-
-}, function(error, userData) {
-  if (error) {
-    console.log("Error creating user:", error);
-  } 
-  else {
-    console.log("Successfully created user account with uid:", userData.uid);
-	database.child("users").push().set({
-     	email: String(email),
-		password: String(password),
-		firstname: String(fname),
-		lastname: String(lname)
-
-     });
-	 
-     
-	 
-	    database.child("userIDAdmin").push().set({
-			email: String(email),
-			password: String(password),
-			firstname: String(fname),
-			lastname: String(lname)
+var result;
+firebase.database().ref("users/").orderByChild("email").equalTo(String(email)).on("child_added",function(snapshot){
+						result= snapshot.val().email;
+	if (result===email){
+		location.reload();
 		
+	}
+	else{
+		firebase.auth().createUserWithEmailAndPassword(String(email),String(pwd)).catch(function(error){
+		var  errorCode=error.code;
+		var errorMessage =error.message;
+		console.log(error.code);
+		console.log(error.message);
+		});
 		
-     });
+		}
+});
+});
+});
+   // console.log("Successfully created user account with uid:", userData.uid);
+	//var childName= email.replace(".",",");
+	
+	
+	//database.child("users").child(String(childName)).set({
+     //	email: String(email),
+	//	pwd: String(pwd),
+		//firstname: String(fname),
+	//	lastname: String(lname),
+		//});
+	 
+	 
 	//window.location.href = "login.html";
-  }
-  
-}); 
+
+
+ 
 
 
 
@@ -84,5 +200,5 @@ password: String(password)
 
 
 
-});
-});
+
+
